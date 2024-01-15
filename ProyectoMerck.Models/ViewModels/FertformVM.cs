0fc192ed5816070a049a4389a4e57cfca52eb51a
Kitesalet <104630744+Kitesalet@users.noreply.github.com>
2024-01-15 18:20:47 -0300

    Cambios en base a la creación de un Fertform Controller, HomeController, asi como tambien la programación y diseño de sus dos vistas prinpales junto con los metodos correspondientoes  necesarios

---
+++ ProyectoMerck.Models/ViewModels/FertformVM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoMerck.Models.ViewModels
+{
+    public class FertformVM
+    {
+
+        public string? FirstFertilityAge { get; set; }
+
+        public string? CurrentAge { get; set; }
+
+
+
+    }
+}
