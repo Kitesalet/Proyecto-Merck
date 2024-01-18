5979f4fe0c0c6b641e160e5decc637e645ce15ae
Kitesalet <104630744+Kitesalet@users.noreply.github.com>
2024-01-18 12:32:31 -0300

    cambio mapper

--- ProyectoMerck.Utilities/mapper.cs
+++ ProyectoMerck.Utilities/mapper.cs
@@ -1,4 +1,7 @@
 ﻿using AutoMapper;
+using ProyectoMerck.Business.DTOs;
+using ProyectoMerck.DataAccess.DTOs;
+using ProyectoMerck.Models.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,7 +15,17 @@ namespace ProyectoMerck.Utilities
 
         public Mapper()
         {
-            
+            //Province
+            CreateMap<GetProvinceDto, Province>().ReverseMap();
+
+            //Consultation
+            CreateMap<GetConsultationDto, Consultation>().ReverseMap();
+
+            //Clinic
+            CreateMap<GetClinicDto, Clinic>().ReverseMap();
+
+            //Location
+            CreateMap<GetLocationDto, Location>().ReverseMap();
 
 
         }
