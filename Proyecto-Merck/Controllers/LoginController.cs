6792eb95d4a22d6deb882c544b1f298a64ba3c67
Kitesalet <104630744+Kitesalet@users.noreply.github.com>
2024-01-24 18:02:41 -0300

    Modificacion botones views varias, Login, Regex, spinner

--- Proyecto-Merck/Controllers/LoginController.cs
+++ Proyecto-Merck/Controllers/LoginController.cs
@@ -1,17 +1,21 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using ProyectoMerck.DataAccess.Interfaces;
 using ProyectoMerck.Models.ViewModels;
+using ProyectoMerck.Utilities;
 
 namespace Proyecto_Merck.Controllers
 {
     public class LoginController : Controller
     {
         private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly IRegexHelper _regexHelper;
 
-        public LoginController(SignInManager<IdentityUser> user)
+        public LoginController(SignInManager<IdentityUser> user, IRegexHelper regexHelper)
         {
 
             _signInManager = user;
+            _regexHelper = regexHelper;
 
         }
 
@@ -19,10 +23,17 @@ namespace Proyecto_Merck.Controllers
         public async Task<IActionResult> Login(LoginVM model)
         {
 
+            if (!_regexHelper.ValidEmail(model.Email))
+            {
+                ModelState.AddModelError("Email", "El email ingresado es invalido!");
+                TempData["Error"] = "El email ingresado es invalido!";
+
+            }
+
             if (ModelState.IsValid)
             {
 
-                var logged = await _signInManager.PasswordSignInAsync(model.Username, model.Password, true, false);
+                var logged = await _signInManager.PasswordSignInAsync(model.Email, model.Password, true, false);
 
                 if(logged.Succeeded)
                 {
@@ -31,16 +42,12 @@ namespace Proyecto_Merck.Controllers
                 else
                 {
                     TempData["Error"] = "El usuario no existe y/o las credenciales son incorrectas";
-                    ModelState.AddModelError("", "El usuario no existe y/o las credenciales son incorrectas");
+                    ModelState.AddModelError("IncorrectCredentials", "El usuario no existe y/o las credenciales son incorrectas");
                 }
 
             }
 
-
             return View("Index",model);
-            
-
-
 
         }
 
