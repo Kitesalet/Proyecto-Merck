a8da723b27276876050c9cf7484acb7bee34c78e
Kitesalet <104630744+Kitesalet@users.noreply.github.com>
2024-02-14 11:11:31 -0300

    Commit final

---
+++ ProyectoMerck.DataAccess/AppMerckContextFactory.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Proyecto_Merck.Areas.Identity.Data;
+
+namespace ProyectoMerck.DataAccess
+{
+
+        public class AppMerckContextFactory : IDesignTimeDbContextFactory<AppMerckContext>
+        {
+        public AppMerckContext CreateDbContext(string[] args)
+        {
+                var path = Path.Combine(Directory.GetCurrentDirectory());
+
+                string parentDirectory = Path.GetDirectoryName(path);
+
+                string realPath = Path.Combine(parentDirectory, "MerckProject");
+
+                Console.WriteLine(realPath);
+
+                var configuration = new ConfigurationBuilder()
+                .SetBasePath(realPath)
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+                var optionsBuilder = new DbContextOptionsBuilder<AppMerckContext>();
+                optionsBuilder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+
+                return new AppMerckContext(optionsBuilder.Options);
+            }
+        }
+
+    
+}
---
+++ ProyectoMerck.DataAccess/Migrations/20240214134805_nuevav22.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProyectoMerck.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class nuevav22 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Consultations",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DateAndtime",
+                value: new DateTime(2024, 2, 14, 10, 48, 4, 640, DateTimeKind.Local).AddTicks(1872));
+
+            migrationBuilder.UpdateData(
+                table: "Consultations",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "DateAndtime",
+                value: new DateTime(2024, 2, 14, 10, 48, 4, 640, DateTimeKind.Local).AddTicks(2012));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Consultations",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DateAndtime",
+                value: new DateTime(2024, 2, 14, 10, 32, 18, 305, DateTimeKind.Local).AddTicks(9611));
+
+            migrationBuilder.UpdateData(
+                table: "Consultations",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "DateAndtime",
+                value: new DateTime(2024, 2, 14, 10, 32, 18, 305, DateTimeKind.Local).AddTicks(9700));
+        }
+    }
+}
