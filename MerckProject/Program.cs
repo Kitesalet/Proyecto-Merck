d6da05a1938a43032d44712afe89f2f3a3f99d46
Administrator <Administrator@DESKTOP-QKT5I78>
2024-05-27 15:13:27 -0300

    cambieros estos

--- MerckProject/Program.cs
+++ MerckProject/Program.cs
@@ -63,7 +63,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Fertform}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 IWebHostEnvironment env = app.Environment;
 Rotativa.AspNetCore.RotativaConfiguration.Setup(env.WebRootPath, "../Rotativa/Windows");
